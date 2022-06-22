b68a8dcc760e444d7a4978beebeca4bad85acb07
Fernanda Schneider <veludoudigrudi@gmail.com>
2022-06-22 09:11:48 -0300

    feito até exercicio 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1/scripts.js
@@ -1,3 +1,4 @@
+//exercicio 1
 let a = 3
 let b = 7
 console.log(a + b);
@@ -5,3 +6,28 @@ console.log(a - b);
 console.log(a * b);
 console.log(a / b);
 console.log(a % b);
+
+//exercicio 2
+const primeira = 46
+const segunda = 87
+
+console.log(primeira > segunda)
+
+//exercicio 3 
+const num1 = 78
+const num2 = 6
+const num3 = 14
+
+if(num1 >= num2 && num1 >= num3){
+    bigger = num1;
+}
+else if(num2 <= num1 && num2 >= num3){
+    bigger = num2;
+}
+else{
+    bigger = num3;
+}
+
+console.log(bigger);
+
+
