c7de6530fcf395337bc1facd8a2b008d46326035
Fernanda Schneider <veludoudigrudi@gmail.com>
2022-06-26 09:43:52 -0300

    exercicio 2 resolvido

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3/exercicios-dia-3.js
@@ -0,0 +1,23 @@
+// exercicio 1
+
+let factorial = 1;
+
+for (let index = 1; index <= 10; index += 1) {
+    factorial = factorial * index;
+}
+
+console.log(index);
+
+//exercicio 2
+
+let word = 'tryber';
+let reverse = '';
+
+for (let index = word.length - 1; index >= 0; index -= 1){
+    reverse = reverse + word[index];
+}
+
+console.log(reverse);
+
+//exercicio 3
+
